11f994fbceacf975808436e2baa6b728c418cd17
subham31p <subhampatro3102@gmail.com>
2023-11-10 13:02:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def subham(request):
+    return render(request,'1.html')
+def subhamop(request):
+    return render(request,'2.html')
+def subhampp(request):
+    return render(request,'3.html')
+def subhampatro(request):
+    return render(request,'4.html')
